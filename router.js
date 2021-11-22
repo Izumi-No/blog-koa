037d811c5e4e8d0c34b848c76148934c6eb925a7
kevyn santos <67838060+Izumi-No@users.noreply.github.com>
2021-11-22 00:01:24 +0000

    começo da rest

--- router.js
+++ router.js
@@ -1,34 +1,49 @@
 const Router = require('koa-router');
 const router = new Router
 
-const { PrismaClient, Prisma } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-router.post("/signup", async (ctx) =>{
-	
-	let data = ctx.request.body
-	let {username} = data
+const bcrypt = require('bcrypt')
+
+router.post("/signup", async (ctx) => {
+	let { username, password } = ctx.request.body
+
+	const salt = await bcrypt.genSalt(10)
+	const passHash = await bcrypt.hash(password.toString(), salt)
 
 	const result = await prisma.user.create({
-    data: {
-      username
-    },
-  })
+		data: {
+			username,
+			passHash
+		},
+	})
 	ctx.body = result
+})
+router.post("/signin", async (ctx) => {
+	let { username } = ctx.request.body
+
+	const user = await prisma.user.findUnique({
+		where: {
+			username
+		}
+	})
+
+	ctx.body = user
 })
 
 
 router.get("/", async (ctx) => {
 	ctx.state = {
-		users: [{name: "jão"}, {name: "cu"}]
+		users: [{ name: "jão" }, { name: "cu" }]
 	}
 	await ctx.render("index")
 })
 
 router.get("/login", async (ctx) => {
 	ctx.state = {
-		users: [{name: "jão"}, {name: "cu"}]
+		users: [{ name: "jão" }, { name: "cu" }]
 	}
 	await ctx.render("login")
 })
